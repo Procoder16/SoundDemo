ca874216d8a04455f72e275330836448e613aad4
Soumik Mukherjee <smsoumik1608@gmail.com>
2020-10-05 21:52:17 +0530

    Slider added

--- app/src/main/java/com/soumik/sounddemo/MainActivity.java
+++ app/src/main/java/com/soumik/sounddemo/MainActivity.java
@@ -2,16 +2,20 @@ package com.soumik.sounddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer harley;
 
+    AudioManager audioManager;
+
     public void play(View view){
-       
+
         harley.start();
     }
     public void pause(View view){
@@ -23,6 +27,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audioManager =(AudioManager)getSystemService (AUDIO_SERVICE);
+        final int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
         harley = MediaPlayer.create(this, R.raw.harley);
+
+        SeekBar volume = (SeekBar) findViewById(R.id.volumeSlider);
+
+        volume.setMax(maxVolume);
+
+        volume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
+                 audioManager.setStreamVolume(audioManager.STREAM_MUSIC, i,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
